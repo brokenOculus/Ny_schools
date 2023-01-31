a328e01dca4fe833ad6148192de29dff9688b478
Eric <51395968+brokenOculus@users.noreply.github.com>
2023-01-31 10:02:41 -0600

    Add files via upload

---
+++ 20230130-EricBrunson-NYC-Schools/app/src/main/java/com/example/nyc_school/api/SchoolApiService.kt
@@ -0,0 +1,19 @@
+package com.example.nyc_school.api
+
+import com.example.nyc_school.model.NYCSchool
+import com.example.nyc_school.model.NYCScore
+import com.example.nyc_school.utils.SCHOOL_ENDPOINT
+import com.example.nyc_school.utils.SCORE_ENDPOINT
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface SchoolApiService {
+    @GET(SCHOOL_ENDPOINT)
+    suspend fun fetchNYCSchools(): Response<List<NYCSchool>>
+
+    @GET(SCORE_ENDPOINT)
+    suspend fun fetchNYCScore(
+        @Query("dbn") dbn: String
+    ): Response<List<NYCScore>>
+}
---
+++ 20230130-EricBrunson-NYC-Schools/app/src/main/java/com/example/nyc_school/hilt/SchoolModule.kt
@@ -0,0 +1,48 @@
+package com.example.nyc_school.hilt
+
+import com.example.nyc_school.api.SchoolApiService
+import com.example.nyc_school.api.SchoolRepository
+import com.example.nyc_school.api.SchoolRepositoryImpl
+import com.example.nyc_school.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class SchoolModule {
+
+    @Singleton
+    @Provides
+    fun provideSchoolApiService(): SchoolApiService =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient())
+            .build()
+            .create(SchoolApiService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideSchoolRepository(): SchoolRepository =
+        SchoolRepositoryImpl(provideSchoolApiService())
+}
